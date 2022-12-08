dd01e0720d83e82877d36f2093efc38e21d1cbdc
irfanysoydan <irfanysoydan@gmail.com>
2022-12-08 18:56:09 +0300

    feat: User endpoints created

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,5 +3,7 @@ const UserRouter = require("express").Router();
 
 UserRouter.get("/", UserController.getAllUsers);
 UserRouter.get("/:id", UserController.getUserById);
+UserRouter.put("/:id", UserController.updateUser);
+UserRouter.delete("/:id", UserController.deleteUser);
 
 module.exports = UserRouter;
