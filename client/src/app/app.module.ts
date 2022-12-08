c85195fb98670b9af6585390a0b1ec6422e9fe0e
rasenshuriken <omerozoglu.1@gmail.com>
2022-12-08 18:55:06 +0300

    feat: Create card component.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    MainPageComponent
+    MainPageComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
