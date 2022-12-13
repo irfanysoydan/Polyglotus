7fbe81c58dc05ddccab0da7246a708cf92b8e55e
rasenshuriken <omerozoglu.1@gmail.com>
2022-12-13 14:05:07 +0300

    feat: Create deck detail.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { DeckComponent } from './components/deck/deck.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent,
+  },
+  {
+    path: 'deck', component: DeckComponent,
   },
   {
     path: 'admin', component: AdminComponent,
@@ -20,4 +24,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
