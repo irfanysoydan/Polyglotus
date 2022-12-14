d81edd638a219738428a87ec06dcf7ee34396f45
irfanysoydan <irfanysoydan@gmail.com>
2022-12-14 18:09:44 +0300

    feat: Add forgot password

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,5 +3,7 @@ const AuthRouter = require("express").Router();
 
 AuthRouter.post("/register", AuthController.createUser);
 AuthRouter.post("/login", AuthController.loginUser);
+AuthRouter.post("/forgotPassword", AuthController.forgotPassword);
+AuthRouter.get("/resetPassword", AuthController.resetPassword);
 
 module.exports = AuthRouter;
