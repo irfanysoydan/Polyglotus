d0c22f187d7b2632f82acf8d0a2cb054a323f6bf
Ahmet KAYMAKCI <ahmetkaymak2207@gmail.com>
2022-12-14 16:39:33 +0300

    feat: Create getDecks function.

--- client/src/app/services/deck.service.ts
+++ client/src/app/services/deck.service.ts
@@ -1,13 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { Deck } from '../models/deck.model';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTY3MTAyNDg5MiwiZXhwIjoxNjcxMDI1ODkyfQ.eKn4cqJCurLhD7eVr0A6WfcAaQIegudizMdkOZOFxLM"
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeckService {
 
+  decksUrl = 'http://10.138.133.93:3000';
+
   constructor(private http: HttpClient) { }
+
+  getDecks(): Observable<Deck[]> {
+    return this.http
+      .get<Deck[]>(this.decksUrl+"/decks",httpOptions)
+  }
 }
