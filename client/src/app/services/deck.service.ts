eade98090ef9bd1fd89459800a9b7dcac3c755a5
Ahmet KAYMAKCI <ahmetkaymak2207@gmail.com>
2022-12-14 17:37:03 +0300

    feat: Create deck Id fuction

--- client/src/app/services/deck.service.ts
+++ client/src/app/services/deck.service.ts
@@ -1,29 +1,31 @@
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Deck } from '../models/deck.model';
 
-
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
-    'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTY3MTAyNDg5MiwiZXhwIjoxNjcxMDI1ODkyfQ.eKn4cqJCurLhD7eVr0A6WfcAaQIegudizMdkOZOFxLM"
-  })
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTY3MTAyODEzMywiZXhwIjoxNjcxNjMyOTMzfQ.NX6dHizhKMD9dKfOUoeNMQLqkYtZjE2XL1mGSgsnkxQ',
+  }),
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DeckService {
-
   decksUrl = 'http://10.138.133.93:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getDecks(): Observable<Deck[]> {
-    return this.http
-      .get<Deck[]>(this.decksUrl+"/decks",httpOptions)
+    return this.http.get<Deck[]>(this.decksUrl + '/decks', httpOptions);
+  }
+
+  getDeckId(id: number): Observable<Deck> {
+    return this.http.get<Deck>(this.decksUrl + '/decks/' + id, httpOptions);
   }
 }
