cc1a91283992e49109219225678fad88369bf694
rasenshuriken <omerozoglu.1@gmail.com>
2022-12-18 15:10:29 +0300

    fix: Register error message

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -10,27 +10,47 @@ import { LoaderService } from 'src/app/services/loader.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(private authService: AuthService, private router: Router, private loader: LoaderService) { }
+  isError: boolean = false;
+  message: string = "";
+  private emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   register(fullname: string, email: string, pass: string) {
-    this.loader.setLoading(true);
     if (typeof fullname != 'undefined' && typeof email != 'undefined' && typeof pass != 'undefined') {
-      let user: User = new User();
-      user.fullName = fullname;
-      user.email = email;
-      user.password = pass;
-      this.authService.registerUser(user).subscribe(result => {
-        if (typeof result != 'undefined') {
-          this.router.navigate(['/login']);
-        }
-        this.loader.setLoading(false);
-      });
-    } else {
-
+      if (fullname.trim() == '') {
+        this.message = "Ad soyad boş olamaz."
+        this.isError = true;
+      }
+      else if (email.trim() == '') {
+        this.message = "Email boş olamaz."
+        this.isError = true;
+      }
+      else if (!this.emailRegex.test(email)) {
+        this.message = "Email geçerli değil."
+        this.isError = true;
+      }
+      else if (pass.trim() == '') {
+        this.message = "Şifre boş olamaz."
+        this.isError = true;
+      }
+      else if (pass.length < 8) {
+        this.message = "Şifre en az 8 karakter olmalıdır."
+        this.isError = true;
+      }
+      else {
+        let user: User = new User();
+        user.fullName = fullname;
+        user.email = email;
+        user.password = pass;
+        this.authService.registerUser(user).subscribe(result => {
+          if (typeof result != 'undefined') {
+            this.router.navigate(['/login']);
+          }
+        });
+      }
     }
   }
 
