d52f56c7ccad47bddbffb89c52e585cb37b2e415
rasenshuriken <omerozoglu.1@gmail.com>
2022-12-19 14:53:56 +0300

    refactor: Update renew password component

--- client/src/app/components/renew-password/renew-password.component.ts
+++ client/src/app/components/renew-password/renew-password.component.ts
@@ -7,24 +7,35 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './renew-password.component.html',
   styleUrls: ['./renew-password.component.scss']
 })
-export class RenewPasswordComponent implements OnInit {
-
-  constructor(private authService: AuthService, private route: ActivatedRoute) {
-
-  }
+export class RenewPasswordComponent {
   paramsObject: any;
   token: string = "";
-  ngOnInit(): void {
+  isError: boolean = false;
+  message: string = "";
+  constructor(private authService: AuthService, private route: ActivatedRoute) { }
 
-  }
   resetPass(pass: string, repass: string) {
+    if (pass.trim() == "" || repass.trim() == "") {
+      this.message = "Şifre boş olamaz"
+      this.isError = true
+    } else if (pass.length < 8 || repass.length < 8) {
+      this.message = " Şifre uzunlukları en az 8 karakter olmalı"
+      this.isError = true
+    } else if (pass !== repass) {
+      this.message = " Şifreler eşleşmiyor"
+      this.isError = true
+    }
     this.route.queryParamMap
       .subscribe((params) => {
         this.paramsObject = { ...params.keys, ...params };
         this.token = this.paramsObject.params.token;
-        this.authService.resetPassword(pass, this.token).subscribe(result => {
-          console.log(result);
+        this.authService.resetPassword(pass, this.token).subscribe(response => {
+          if (response.isSuccessful) {
 
+          } else {
+            this.message = " Şifre değiştirilemedi"
+            this.isError = true;
+          }
         });
       }
       );
